39932c441dd124df662d2011b892b656bb951430
Kumar Singh, Randhir <rkunv@allstate.com>
2021-05-01 11:25:23 +0530

    Added TableViewCell

---
+++ Lenskart/MovieListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListTableViewCell.swift
+//  Lenskart
+//
+//  Created by Kumar Singh, Randhir on 01/05/21.
+//
+
+import UIKit
+
+class MovieListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
